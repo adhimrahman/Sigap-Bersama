094aa1dcf9a72b4b6ac2b3e0e267c2e0cc4d5809
adhimrahman <adhimrahaman@gmail.com>
2024-12-01 21:49:08 +0800

    add: individu page

---
+++ src/pages/individu.tsx
@@ -0,0 +1,19 @@
+
+
+export default function Individu() {
+	const kocak = 20
+
+	return(
+		<div className="bg-red-500 text-black flex flex-col justify-center items-center w-full h-[500px]">
+			<h1>apa {kocak}</h1>
+			<p>kocak</p>
+			<p>kocak</p>
+			<p>kocak</p>
+			<p>kocak</p>
+			<p>kocak</p>
+			<p>kocak</p>
+			<p>kocak</p>
+		</div>
+	)
+
+}
