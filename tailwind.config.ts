a39ca0b3ce7ac143cdf9992919ad3e527fb23129
adhimrahman <adhimrahaman@gmail.com>
2024-12-01 21:44:07 +0800

    add: ext col ijoTua & ijoMuda

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,28 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		// "./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				ijoTua: "var(--ijoTua)",
+				ijoMuda: "var(--ijoMuda)"
+			},
 
-      backgroundImage: {
-        'loginForm': "url('/bege.png')"
-      }
-    },
-  },
-  plugins: [],
+			boxShadow: {
+				'3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+			},
+
+			backgroundImage: {
+				'loginForm': "url('../../public/images/bege.png')"
+			}
+		},
+	},
+	plugins: [],
 } satisfies Config;
