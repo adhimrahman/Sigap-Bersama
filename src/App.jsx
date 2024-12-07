bca46fccb7d2d9b09d6659495e5f3f895b272cfc
adhimrahman <adhimrahaman@gmail.com>
2024-12-07 13:18:28 +0800

    add: bencana endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/page";
 import Limbah from "./pages/Limbah/page";
+import Bencana from "./pages/Bencana/page";
 import './index.css';
 
 const App = () => (
 	<Router>
 		<Routes>
 			<Route path="/" element={<Home />} />
-			<Route path="/limbah" element={<Limbah />} />
+			<Route path="/limbah" element={<Limbah />} /> 
+			<Route path="/bencana" element={<Bencana />} /> 
 		</Routes>
 	</Router>
 );
