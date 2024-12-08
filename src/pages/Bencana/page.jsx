f2b33908e98c414426801f9dc5c0b24372523bd0
adhimrahman <adhimrahaman@gmail.com>
2024-12-09 00:40:54 +0800

    fix: err fetching datas

--- src/pages/Bencana/page.jsx
+++ src/pages/Bencana/page.jsx
@@ -1,8 +1,31 @@
-import Content from "../../assets/bege.png"
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { firestore } from "../../api/firebaseConfig";
 
 import Navbar from "../../components/layouts/Navbar";
+import Content from "../../assets/bege.png"
 
 export default function Bencana() {
+    const [bencanaData, setBencanaData] = useState([]);
+
+    useEffect(() => {
+        const fetchBencana = async () => {
+            try {
+                const bencanaCollection = collection(firestore, "bencana")
+                const bencanaSnapshot = await getDocs(bencanaCollection)
+
+                const bencanaList = bencanaSnapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data()
+                }))
+
+                setBencanaData(bencanaList)
+            } catch(err) {
+                console.log("tolong jangan error : ", err)
+            }
+        };
+        fetchBencana(); // call function saat komponen di-load
+    }, []);           
     
     return (
         <>
@@ -23,28 +46,28 @@ export default function Bencana() {
                 <input type="text" className="w-full max-w-2xl p-4 rounded-lg shadow-md outline-none" placeholder="Search..." />
             </div>
             <div className="cards grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {Array.from({ length: 12 }).map((_, index) => (
-                    <div key={index} className="card bg-white rounded-lg shadow-2xl drop-shadow-2xl hover:cursor-pointer hover:scale-[1.01]">
-                        <div className="h-40 bg-gray-200 rounded-t-lg overflow-hidden">
-                            <img src={Content} alt="" />
+                {bencanaData.map((item) => (
+                    <div key={item.id} className="card bg-white rounded-lg shadow-2xl hover:cursor-pointer hover:scale-[1.01]">
+                        <div className="bg-gray-200 rounded-t-lg overflow-hidden">
+                            <img src={Content} alt="" className="w-fit h-fit" />
                         </div>
                         <div className="p-4">
-                            <h2 className="text-lg font-bold">Nama Campaign</h2>
-                            <p className="text-sm text-gray-600">Nama Komunitas</p>
+                            <h2 className="text-lg font-bold">{item.name}</h2>
+                            <p className="text-sm text-gray-600">{item.creator}</p>
 
                             <div className="mt-4">
-                                <div className="top flex">
-                                    <svg className="w-4 h-4 text-gray-400 mr-2" fill="none" stroke="green" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                    </svg>
-                                    <p className="flex items-center text-sm text-gray-600">Tanggal</p>
-                                </div>
-                                <div className="bottom flex mt-2 pb-2">
-                                    <svg className="w-4 h-4 text-gray-400 mr-2" fill="none" stroke="green" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z"/><circle cx="12" cy="10" r="3"/>
-                                    </svg>
-                                    <p className="flex items-center text-sm text-gray-600">Lokasi</p>
-                                </div>
+                                    <p className="flex items-center text-sm text-gray-600">
+                                        <svg className="w-4 h-4 text-gray-400 mr-2" fill="none" stroke="green" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                        </svg>
+                                        {item.date}
+                                    </p>
+                                    <p className="flex items-center text-sm text-gray-600">
+                                        <svg className="w-4 h-4 text-gray-400 mr-2" fill="none" stroke="green" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z"/><circle cx="12" cy="10" r="3"/>
+                                        </svg>
+                                        {item.locate}
+                                    </p>
                             </div>
                         </div>
                     </div>
