72e707f956d8d63213f66720756bbc05d3b1362b
Andika Arif Rahman <andikarahman00925@gmail.com>
2024-12-08 15:04:01 +0800

    feat: add content for bencanadetail page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home/page";
 import Limbah from "./pages/Limbah/page";
 import LimbahDetail from "./pages/Limbah/details";
 import Bencana from "./pages/Bencana/page";
+import BencanaDetail from "./pages/Bencana/details";
 import './index.css';
 
 const App = () => (
@@ -12,6 +13,7 @@ const App = () => (
 			<Route path="/limbah" element={<Limbah />} /> 
 			<Route path="/limbahdetail" element={<LimbahDetail />} />{/* Untuk preview contoh halaman detail, nanti diubah routenya*/}
 			<Route path="/bencana" element={<Bencana />} /> 
+			<Route path="/bencanadetail" element={<BencanaDetail />} />{/* Untuk preview contoh halaman detail, nanti diubah routenya*/}
 		</Routes>
 	</Router>
 );
