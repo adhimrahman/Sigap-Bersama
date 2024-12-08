651e5bf3834a634c5e88db3f3fcf773c5b6f46fe
Andika Arif Rahman <andikarahman00925@gmail.com>
2024-12-08 14:38:13 +0800

    feat: add content for limbahdetail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/page";
 import Limbah from "./pages/Limbah/page";
+import LimbahDetail from "./pages/Limbah/details";
 import Bencana from "./pages/Bencana/page";
 import './index.css';
 
@@ -9,6 +10,7 @@ const App = () => (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/limbah" element={<Limbah />} /> 
+			<Route path="/limbahdetail" element={<LimbahDetail />} />{/* Untuk preview contoh halaman detail, nanti diubah routenya*/}
 			<Route path="/bencana" element={<Bencana />} /> 
 		</Routes>
 	</Router>
