c59b44a39ead84d48d81e63b0aa33b57a802cc0b
adhimrahman <adhimrahaman@gmail.com>
2024-12-08 11:16:49 +0800

    add: custom height 100

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,9 @@ export default {
 				DEFAULT: '2px 2px 4px rgba(0, 0, 0, 0.5)',
 				lg: '3px 3px 6px rgba(0, 0, 0, 0.5)',
 				custom: '2px 4px 8px rgba(255, 0, 0, 0.75)',
+			},
+			height: {
+				'100': "26rem"
 			}
 		},
 	},
