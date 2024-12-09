39cb9dfc83297acc2f94844c3069d7cab9153315
adhimrahman <adhimrahaman@gmail.com>
2024-12-10 00:08:59 +0800

    fix: limbah section

--- src/pages/Home/page.jsx
+++ src/pages/Home/page.jsx
@@ -79,11 +79,9 @@ export default function LandingPage() {
                     label === 'Maps' ? 'maps' : label === 'Contact Us' ? 'footer' : 'header'
                 const targetElement = document.querySelector(`.${targetClass}`);
                 targetElement?.scrollIntoView({ behavior: 'smooth' });
-            }}
-            />
+            }}/>
 
             <main className="w-full flex flex-col bg-[#F0F0F0]">
-
                 <div className="header w-full h-screen bg-loginForm bg-center bg-cover flex flex-col justify-center items-center gap-y-12">
                     <div className="top w-full h-fit flex justify-center items-center">
                         <p className="text-white shine text-3xl sm:text-6xl md:text-6xl lg:text-8xl font-extrabold uppercase tracking-wider mt-36 text-shadow-green-400 text-shadow-x-xl">sigap bersama</p>
@@ -204,35 +202,36 @@ export default function LandingPage() {
                         </div>
 
                         <div className="bottomSide w-4/5 pt-5 rounded-b-lg hidden lg:grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 overflow-hidden gap-6">
-                            {limbahData.map((item) => (
-                                <div key={item.id} className="card bg-white rounded-lg shadow-2xl drop-shadow-2xl hover:cursor-pointer hover:scale-[1.01]">
-                                    <div className="h-40 bg-gray-200 rounded-t-lg overflow-hidden">
-                                        <img src={item.image} alt="" />
-                                    </div>
-                                    <div className="p-4">
-                                        <h2 className="text-lg font-bold capitalize">{item.name}</h2>
-                                        <p className="text-sm text-gray-600 capitalize">{item.creator}</p>
+                        {limbahData.map((item) => (
+                            <div key={item.id} className="card bg-white rounded-lg shadow-2xl drop-shadow-2xl hover:cursor-pointer hover:scale-[1.01]">
+                                <div className="h-40 bg-gray-200 rounded-t-lg overflow-hidden">
+                                    <img src={item.image} alt="" className="h-full w-full object-cover" />
+                                </div>
+                                <div className="p-4">
+                                    <h2 className="text-lg font-bold capitalize">{item.name}</h2>
+                                    <p className="text-sm text-gray-600 capitalize">{item.creator}</p>
 
-                                        <div className="mt-4">
-                                            <div className="top flex">
-                                                <svg className="w-4 h-4 text-gray-400 mr-2" fill="none" stroke="green" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                                </svg>
-                                                <p className="flex items-center text-sm text-gray-600">{item.date}</p>
-                                            </div>
-                                            <div className="bottom flex mt-2 pb-2">
-                                                <svg className="w-4 h-4 text-gray-400 mr-2" fill="none" stroke="green" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z"/><circle cx="12" cy="10" r="3"/>
-                                                </svg>
-                                                <p className="flex items-center text-sm text-gray-600">{item.locate}</p>
-                                            </div>
+                                    <div className="mt-4">
+                                        <div className="top flex">
+                                            <svg className="w-4 h-4 text-gray-400 mr-2" fill="none" stroke="green" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                            </svg>
+                                            <p className="flex items-center text-sm text-gray-600">{item.date}</p>
+                                        </div>
+                                        <div className="bottom flex mt-2 pb-2">
+                                            <svg className="w-4 h-4 text-gray-400 mr-2" fill="none" stroke="green" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z"/><circle cx="12" cy="10" r="3"/>
+                                            </svg>
+                                            <p className="flex items-center text-sm text-gray-600">{item.locate}</p>
                                         </div>
                                     </div>
                                 </div>
-                            ))}
+                            </div>
+                        ))}
+                
                         </div>
-                        <button className="w-44 h-11 rounded-lg bg-ijoTua text-white capitalize mt-7 hover:opacity-80"
-                            onClick={() => navigate('/limbah')}>
+                        <button className="w-44 h-11 rounded-lg bg-ijoTua hover:opacity-80 text-white capitalize mt-7"
+                            onClick={() => navigate('/bencana')}>
                             lihat aktivitas lain
                         </button>
                     </div>
