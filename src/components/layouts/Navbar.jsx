63c5f12ac367a77ff54834d1337d587f6d731fe7
adhimrahman <adhimrahaman@gmail.com>
2024-12-13 23:17:20 +0800

    fix: navigation bar logic

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,59 +1,72 @@
-import PropTypes from "prop-types";
-import Logo from "../../assets/logo.png";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import useUser from '../../context/useUser';
-import { signOut } from 'firebase/auth';
-import { auth } from '../../api/firebaseConfig';
+import Logo from '../../assets/logo.png';
+import { auth } from "../../api/firebaseConfig";
 
-export default function Navbar({ scrollHandler, menuItems = [] }) {
+function Navbar() {
     const navigate = useNavigate();
-    const { user } = useUser();
-    const { setUser } = useUser();
+    const { user } = useUser(); // Mendapatkan data user dari UserContext
 
-    const handleSignout = async () => {
-        try {
-            await signOut(auth); // Sign out dari Firebase
-            setUser(null); // Hapus data user dari context
-            navigate('/'); // Arahkan kembali ke halaman utama
-        } catch (error) { console.error("Error signing out:", error) }
+    const scrollToElement = (className) => {
+        const targetElement = document.querySelector(`.${className}`);
+        if (targetElement) { targetElement.scrollIntoView({ behavior: 'smooth' }) }
+    };
+
+    const template = [
+        { label: 'Home', onClick: () => scrollToElement('header') },
+        { label: 'About', onClick: () => scrollToElement('about') },
+        { label: 'Bencana', onClick: () => scrollToElement('bencana') },
+        { label: 'Limbah', onClick: () => scrollToElement('limbah') },
+        { label: 'Maps', onClick: () => scrollToElement('maps') },
+        { label: 'Contact Us', onClick: () => scrollToElement('footer') },
+    ]
+
+    const guestMenu = [
+        ...template,
+        { label: 'Sign In', onClick: () => navigate('/signin') },
+    ];
+
+    const komunitasMenu = [
+        ...template,
+        { label: 'My Event', onClick: () => navigate('/myevent') },
+        { label: 'Sign Out', onClick: handleSignOut },
+    ];
+
+    const individuMenu = [
+        ...template,
+        { label: 'My Interest', onClick: () => navigate('/myinterest') },
+        { label: 'Sign Out', onClick: handleSignOut },
+    ];
+
+    function handleSignOut() {
+        auth.signOut()
+            .then(() => {
+                console.log('User signed out.');
+                navigate('/signin');
+            })
+            .catch((error) => console.error('Error signing out:', error));
     }
 
+    const menuItems =
+        user?.role === 'komunitas' ? komunitasMenu :
+        user?.role === 'individu' ? individuMenu : guestMenu;
+
     return (
-        <nav className="flex items-center justify-between px-24 py-4 bg-ijoTua fixed top-0 w-full h-20 z-10 shadow-lg">
-            <div className="logo flex items-center hover:cursor-pointer" onClick={() => navigate('/')}>
-                <img src={Logo} alt="Logo OKFT" className="w-8 md:w-12 drop-shadow-2xl shadow-2xl" />
-                <span className="ml-3 text-xl md:text-2xl font-bold text-white uppercase">sigap bersama</span>
+        <nav className="flex items-center justify-between px-24 py-4 bg-ijoTua fixed top-0 w-full h-16 z-10 shadow-lg">
+            <div className="logo flex items-center cursor-pointer" onClick={() => scrollToElement('home')}>  
+                <img src={Logo} alt="Logo" className="w-10 drop-shadow-xl" />
+                <span className="ml-3 text-lg font-bold text-white uppercase">Sigap Bersama</span>
             </div>
 
-            <ul className="hidden md:flex space-x-8 text-white text-sm md:text-base font-medium">
+            <ul className="flex space-x-6 text-white text-sm font-medium">
                 {menuItems.map((item, index) => (
-                    <li key={index} className="relative cursor-pointer hover:text-red-400 transition duration-300"
-                    onClick={() => {
-                        if (['Home', 'About', 'Bencana', 'Limbah', 'Testimoni', 'Maps', 'Contact Us'].includes(item)) { scrollHandler(item) }
-                        else if (item === 'My Event') { navigate('/myEvent') }
-                    }}>{item}
+                    <li key={index} className="cursor-pointer hover:text-red-400 transition duration-300" onClick={item.onClick}>
+                        {item.label}
                     </li>
                 ))}
-                {user ? (
-                    <>
-                    <li onClick={() => navigate('/profile')} className="relative cursor-pointer hover:text-red-400 transition duration-300">
-                        Profil {/* Profil Menu */}
-                    </li>
-                    <li onClick={handleSignout} className="relative cursor-pointer hover:text-red-400 transition duration-300">
-                        Sign Out {/* Sign Out Menu */}
-                    </li>
-                    </>
-                ) : (
-                    <li onClick={() => navigate('/signin')} className="relative cursor-pointer hover:text-red-400 transition duration-300">
-                        Sign In     {/*Sign In Menu for Guest*/}
-                    </li>
-                )}
             </ul>
         </nav>
     );
 }
 
-Navbar.propTypes = {
-    menuItems: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.object])),
-    scrollHandler: PropTypes.func.isRequired,
-};
+export default Navbar;
