1cee014524d5068472b5d9794cc8bbd448331781
Muhammad Rifqi <muhrifqisirajuddin@gmail.com>
2024-12-14 17:04:19 +0800

    fix

--- src/pages/My/interest.jsx
+++ src/pages/My/interest.jsx
@@ -48,7 +48,7 @@ export default function MyInterest() {
             <Navbar pageKeys={['landingPage', 'navBencana', 'navLimbah', 'contactUs']} />
 
             <div className="w-full px-9 sm:px-12 md:px-12 lg:px-24 p-4 mt-20 mb-12">
-                <h1 className="text-4xl font-bold tracking-wider text-center pt-9 mb-8 capitalize">My Event</h1>
+                <h1 className="text-4xl font-bold tracking-wider text-center pt-9 mb-8 capitalize">My Interest</h1>
                 
                 <SearchBar query={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
                 
