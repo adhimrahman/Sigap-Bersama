349d8eec59ec344f8d8e2914eb888eb9b1426d01
adhimrahman <adhimrahaman@gmail.com>
2024-12-14 16:30:07 +0800

    fix: bg color and spinner size

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
     return (
-        <div className="w-full h-screen flex justify-center items-center bg-gray-100">
-            <div className="w-16 h-16 border-4 border-t-green-500 border-gray-300 rounded-full animate-spin"></div>
+        <div className="w-full h-screen flex justify-center items-center bg-ijoTua/10">
+            <div className="w-20 h-20 border-4 border-t-green-500 border-gray-300 rounded-full animate-spin"></div>
         </div>
     );
 }
