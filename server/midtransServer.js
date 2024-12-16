128021d733db06951fb660bc7281133608d3d133
adhimrahman <adhimrahaman@gmail.com>
2024-12-16 18:41:17 +0800

    fix: test midtrans

--- server/midtransServer.js
+++ server/midtransServer.js
@@ -1,46 +1,46 @@
-import express from "express";
-import midtransClient from "midtrans-client";
-import dotenv from "dotenv";
-import cors from "cors";
+// import express from "express";
+// import midtransClient from "midtrans-client";
+// import dotenv from "dotenv";
+// import cors from "cors";
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
+// const app = express();
 
-// Tambahkan middleware CORS
-app.use(cors({
-    origin: "http://localhost:5173",
-    // origin: "https://sigap-bersama.vercel.app/",
-    methods: "POST",
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+// // Tambahkan middleware CORS
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     // origin: "https://sigap-bersama.vercel.app/",
+//     methods: "POST",
+//     allowedHeaders: ["Content-Type", "Authorization"]
+// }));
 
-app.use(express.json());
+// app.use(express.json());
 
-app.post("/api/midtrans", async (req, res) => {
-    try {
-        const { orderId, amount, name, email } = req.body;
+// app.post("/api/midtrans", async (req, res) => {
+//     try {
+//         const { orderId, amount, name, email } = req.body;
 
-        const snap = new midtransClient.Snap({
-            isProduction: false,
-            // eslint-disable-next-line no-undef
-            serverKey: process.env.MIDTRANS_SERVER_KEY,
-        });
+//         const snap = new midtransClient.Snap({
+//             isProduction: false,
+//             // eslint-disable-next-line no-undef
+//             serverKey: process.env.MIDTRANS_SERVER_KEY,
+//         });
 
-        const parameter = {
-            transaction_details: { order_id: orderId, gross_amount: parseInt(amount) },
-            customer_details: { first_name: name, email: email },
-        };
+//         const parameter = {
+//             transaction_details: { order_id: orderId, gross_amount: parseInt(amount) },
+//             customer_details: { first_name: name, email: email },
+//         };
 
-        const transaction = await snap.createTransaction(parameter);
-        res.status(200).json({ token: transaction.token });
-    } catch (error) {
-        console.error("Midtrans Error Stack:", error);
-        res.status(500).json({ error: "Failed to create transaction", message: error.message });
-    }
-});
+//         const transaction = await snap.createTransaction(parameter);
+//         res.status(200).json({ token: transaction.token });
+//     } catch (error) {
+//         console.error("Midtrans Error Stack:", error);
+//         res.status(500).json({ error: "Failed to create transaction", message: error.message });
+//     }
+// });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server berjalan di http://localhost:${PORT}`);
+// });
